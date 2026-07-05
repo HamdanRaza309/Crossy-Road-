60a9d299bb786490359d57b72b66d6488199ab2b
HamdanRaza309 <githubuse309@gmail.com>
2026-07-06 01:58:46 +0500

    feat: add player death particle effect with collision and explosion animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class Character : MonoBehaviour {
   [SerializeField] private GameManager gameManager;
   [SerializeField] private GameObject character;
+  [SerializeField] private ParticleSystem deathParticles;
 
   private void OnCollisionEnter(Collision collision) {
     // Only collide with vehicles if we're not already done so.
     if (collision.gameObject.CompareTag("Vehicle") && character.activeSelf) {
       // Hide the character model
       character.SetActive(false);
+
+      // Orient the particles relative to the collision.
+      Vector3 collisionPoint = collision.GetContact(0).point;
+      deathParticles.transform.position = collisionPoint;
+      deathParticles.transform.LookAt(transform.position + Vector3.up);
+
+      // Show the particles.
+      deathParticles.Play();
 
       // Tell the GameManager we've collided.
       gameManager.PlayerCollision();
@@ -18,7 +27,8 @@ public class Character : MonoBehaviour {
   public void Reset() {
     // Re-enable the character model.
     character.SetActive(true);
+    // Remove any left over particles.
+    deathParticles.Clear();
   }
-
 }
 
